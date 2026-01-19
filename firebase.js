414ce3c65fb3236d2f81e9d54a76d106ae7281fb
snugstitches <sakshidhiman276@gmail.com>
2026-01-19 13:03:04 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDU77Toyabu_tIUAiP-gvDlFG5b1q_g_h0",
+  authDomain: "snugstiches-c7b49.firebaseapp.com",
+  projectId: "snugstiches-c7b49",
+  storageBucket: "snugstiches-c7b49.firebasestorage.app",
+  messagingSenderId: "626685151678",
+  appId: "1:626685151678:web:ba9cdea744d82a1a1cd1ef",
+  measurementId: "G-FS0Z89ZW5L"
+};
